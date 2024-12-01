62d793c3966501c094dc8bf1260b838685113333
Gokul Suresh s.v <gokulofficial18062@gmail.com>
2024-12-01 08:32:56 +0530

    checking commit december

--- src/controller/subscription.ts
+++ src/controller/subscription.ts
@@ -50,7 +50,7 @@ export const updateSubscribe = async (req:Request, res:Response) => {
 
     return res.status(200).json({ message: "User subscription updated successfully", user });
   } catch (error) {
-    console.error("Error updating subscription section", error);
+    console.error("Error updating subscription section working on it", error);
     return res.status(500).json({ message: "Internal Server Error" });
   }
 };
